ceb70fe01b52d4bfdb85efc5d5d42e9a377fd693
agu chidimma maryfiona <aguchidimmamaryfiona@gmail.com>
2023-08-11 21:05:13 +0100

    zones

---
+++ zone/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Todo from './../src/component/Todo';
+
+
+function App() {
+  return (
+    <>
+    
+    <div>
+      <h1>My Todos</h1>
+      <Todo text='Learn React' />
+    </div>
+    
+    </>
+  );
+}
+
+export default App;
